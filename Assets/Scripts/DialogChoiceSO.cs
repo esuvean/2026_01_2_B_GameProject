e94b5ed76508e1b58e1e3285df41c6327342259c
esuvean <taetae-0@naver.com>
2026-04-08 11:49:17 +0900

    [feat] 카드 데이터 생성 및 기본 카드 기능 제작

--- Assets/Scripts/DialogChoiceSO.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "DialogChoiceSO", menuName = "Scriptable Objects/DialogChoiceSO")]
-public class DialogChoiceSO : ScriptableObject
-{
-    public string text;
-    public int nextld;
-}
